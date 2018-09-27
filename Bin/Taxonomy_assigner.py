ed21a551b3c989b1f4ff6c39b9b3ad55d4a4d5f4
Thomas Hitch <th.tomhitch@gmail.com>
2018-09-27 08:57:50 +0200

    Sorted bia Bitscore
    As BLASTN max_target_seqs is no longer usable, we sort multiple hit to the same sequence by bitscore so only the best match is accepted.

--- Bin/Taxonomy_assigner.py
+++ Bin/Taxonomy_assigner.py
@@ -3,9 +3,17 @@ import sys
 
 
 Taxonomy = {}
+temp = {}
 for line in open(sys.argv[1] + '/Consortium_aligned_seqs.m8','r'):
-    timber = line.split('\t')
-    Taxonomy[timber[0]] = timber[1] + '\n'
+    timber = line.replace('\n','').split('\t')
+    if timber[0] in temp:
+        nbit = float(timber[11])
+        obit = temp[timber[0]][1]
+        if nbit > obit:
+            temp[timber[0]] = [timber[1], nbit]
+            
+for k,v in temp.iteritems():
+    Taxonomy[k] = v[0] + '\n'
     
     
 mapping_names = {}
